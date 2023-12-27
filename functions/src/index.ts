38f230ba0eb4eab4326ca18b8704885ff03d326b
Ryan Thomas <ryanthomascs@gmail.com>
2023-12-26 22:12:27 -0500

    remove getCustomDescription functionality temporarily

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -97,74 +97,16 @@ export const getCustomDescription = onRequest(async (request, response) => {
         }
         const transcript: string = request.body.transcript ?? 'what is my childs name';
         const modelName = request.body.model ?? 'gpt-3.5-turbo';
-        // const cHistory = request.body.chatHistory ?? [];
 
 
         try {
 
             const model = new ChatOpenAI({openAIApiKey, modelName, maxTokens: 200, temperature: 0.3});
             
-            const docs = [new Document({pageContent: transcript})]
-            const splitter = new TokenTextSplitter({
-                chunkSize: 750,
-                chunkOverlap: 250,
-            });
-
-            const docsSummary = await splitter.splitDocuments(docs);
-
-            const summaryTemplate = `
-            You are an expert in creating detailed YouTube descriptions.
-            Your goal is to create a description for a youtube video based on the transcript of the video.
-            Below you find the transcript of a Youtube video:
-            --------
-            {text}
-            --------
-
-            The transcript of the Youtube video will also be used as the basis for a question and answer bot.
-            Provide some examples questions and answers that could be asked about the Video. Make these questions very specific.
-
-            Total output will be a summary of the video and a list of example questions the user could ask of the video.
-
-            SUMMARY AND QUESTIONS:
-            `;
-
-            const SUMMARY_PROMPT = PromptTemplate.fromTemplate(summaryTemplate);
-
-            const summaryRefineTemplate = `
-            You are an expert in creating detailed YouTube descriptions.
-            Your goal is to create a description for a youtube video based on the transcript of the video.
-            We have provided an existing summary up to a certain point: {existing_answer}
-
-            Below you find the transcript of a podcast:
-            --------
-            {text}
-            --------
-
-            Given the new context, refine the summary and example questions.
-            The transcript of the youtube video will also be used as the basis for a question and answer bot.
-            Provide some examples questions and answers that could be asked about the podcast. Make
-            these questions very specific.
-            If the context isn't useful, return the original summary and questions.
-            Total output will be a summary of the video and a list of example questions the user could ask of the video.
-
-            SUMMARY AND QUESTIONS:
-            `;
-
-            const SUMMARY_REFINE_PROMPT = PromptTemplate.fromTemplate(
-            summaryRefineTemplate
-            );
-
-            const summarizeChain = loadSummarizationChain(model, {
-                type: "refine",
-                verbose: true,
-                questionPrompt: SUMMARY_PROMPT,
-                refinePrompt: SUMMARY_REFINE_PROMPT,
-            });
-
-            const summary = await summarizeChain.run(docsSummary);
+            //UNDER CONSTRUCTION
 
             
-            response.send({ summary });
+            response.send('');
 
         } catch (error) {
             console.error('Error reading file:', error);
